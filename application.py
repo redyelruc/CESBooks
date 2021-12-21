cbc29b83d92c85de23fc8dd7b0ee422a0ca16237
Aidan Curley <aidan@Rampage.Home>
2021-12-21 18:02:57 +0000

    add logic to expose registration api

--- application.py
+++ application.py
@@ -5,7 +5,9 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 
-from constants import MAX_BORROWING_DURATION
+from book import Book
+from constants import MAX_BORROWING_DURATION, INVOICES_URL
+from errors import IncompleteBookError
 from fine import Fine
 
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -15,6 +17,8 @@ import isbnlib
 import requests
 
 # Configure application
+from transaction import Transaction
+
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
@@ -44,24 +48,18 @@ db = SQL("mysql://redyelruc:financered180974finance@127.0.0.1:3306/finance")
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
     # Forget any user_id
     session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure student_id was submitted
         if not request.form.get("student_id"):
             return apology("must provide student_id", 403)
-
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for student_id
         rows = db.execute("SELECT * FROM student WHERE id = %s", request.form.get("student_id"))
-
         # Ensure student_id exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid details", 403)
@@ -69,7 +67,6 @@ def login():
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
@@ -77,38 +74,23 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
+# @login_required
+# @app.route("firstlogin")
+# def firstlogin():
+#     """Update Password"""
+
+
+@app.route("/register", methods=["POST"])
 def register():
     """Register user"""
-    if request.method == "GET":
-        # Display form for user to create account
-        return render_template("register.html")
-    else:
-        # register the new user
-        # check student_id is not blank
-        if not request.form.get("student_id"):
-            return apology("must provide student_id", 403)
-        # check name not blank
-        if not request.form.get("name"):
-            return apology('must provide name', 403)
-        # check password is not blank
-        if not request.form.get("password"):
-            return apology('must provide password', 403)
-        # check student_id is unique
-        if db.execute("SELECT * FROM student WHERE id = %s", request.form.get("student_id")):
-            return apology("you already have an account", 403)
-        # check password and confirmation are same
-        if request.form.get("password") != request.form.get("confirm-password"):
-            return apology("password and confirmation do not match", 403)
-        # hash the password and create row in db
-        db.execute("INSERT INTO student(id, name, hash) VALUES (%s, %s, %s)",
-                   request.form.get("student_id"), generate_password_hash(request.form.get("password")))
+    student_id = request.json['student_id']
+    # check student_id is not already registered
+    if db.execute("SELECT * FROM student WHERE id = %s", student_id):
+        return student_id, 204
 
-        # make sure that the new user is logged in
-        rows = db.execute("SELECT * FROM student WHERE id = %s", request.form.get("student_id"))
-        # set the session so we know who is logged in
-        session["user_id"] = rows[0]["id"]
-        return redirect("/")
+    # create row in db with default password
+    db.execute("INSERT INTO student(id, hash) VALUES (%s, %s)", student_id, generate_password_hash("000000"))
+    return student_id, 201
 
 
 @app.route("/logout")
@@ -141,7 +123,8 @@ def books():
 def transactions():
     """Show history of transactions"""
     rows = db.execute(
-        "SELECT * FROM transaction WHERE student_id = %s ORDER BY date_returned ASC, date_borrowed DESC LIMIT 8", session["user_id"])
+        "SELECT * FROM transaction WHERE student_id = %s ORDER BY date_returned ASC, date_borrowed DESC LIMIT 8",
+        session["user_id"])
     transaction_history = []
     for row in rows:
         transaction_history.append([row['date_borrowed'], row['date_returned'], row['book_isbn'],
@@ -179,29 +162,37 @@ def return_books():
     if request.method == "GET":
         return render_template("return.html")
     else:
-        isbn = request.form.get("isbn")
         today = date.today().strftime('%Y-%m-%d')
+        book = select_book(request.form.get("isbn"))
+
+        if not book:
+            flash("This book is not in our database.")
+            return redirect("/books")
+
         # RETURN A BOOK
         try:
-            db.execute("UPDATE transaction SET date_returned = %s WHERE book_isbn = %s AND student_id = %s AND "
-                       "date_returned = %s", today, isbn, session["user_id"], '0000:00:00')
-            db.execute("UPDATE book SET copies = copies + 1 WHERE isbn = %s", isbn)
-            record = db.execute(
-                "SELECT date_borrowed FROM transaction WHERE book_isbn = %s AND student_id = %s AND date_returned = %s",
-                isbn, session["user_id"], date.today())
+            records = db.execute(
+                "SELECT * FROM transaction WHERE book_isbn = %s AND student_id = %s AND date_returned = '0000:00:00'",
+                book.isbn, session["user_id"])
+
+            if len(records) != 1:
+                flash("This is not one of the books you have borrowed.")
+                return redirect("/history")
+
+            transaction = Transaction(records[0])
+
+            db.execute("UPDATE transaction SET date_returned = %s WHERE id = %s", today, transaction.id)
+            db.execute("UPDATE book SET copies = copies + 1 WHERE isbn = %s", book.isbn)
+            message = 'Thanks for your return.'
 
             # check, and issue fine if needed
-            date_borrowed = record[0]["date_borrowed"]
-            if days_between(date_borrowed, date.today()) > MAX_BORROWING_DURATION:
-                fine = Fine(date_borrowed, session["user_id"])
-                print(f'fine: {repr(fine)}')
-                # url = "https://localhost:8081/post"
-                # r = requests.post(url, data=fine.details)
-                # print(r.status_code)
-                # print(r.text)
-                flash(f"Thanks you for your return. You have been fined £{fine.amount}. Please login to the Payment Portal to pay the fine.")
-            else:
-                flash("Thank you for your return!")
+            if days_between(transaction.date_borrowed, date.today()) > MAX_BORROWING_DURATION:
+                fine = Fine(transaction.date_borrowed, session["user_id"])
+                r = requests.post(INVOICES_URL, json=fine.details)
+
+                message += f" You have been fined £{fine.amount}. Please log in to the Payment Portal to pay the " \
+                           f"invoice reference: {r.json()['reference']}. "
+            flash(message)
         except Exception as e:
             print(e)
 
@@ -211,9 +202,8 @@ def return_books():
 @app.route("/add", methods=["GET", "POST"])
 @login_required
 def add():
-    """Get stock quote."""
+    """Add a new title to the library."""
     if request.method == "GET":
-        # Display form for user to enter stock to search
         return render_template("add.html")
     else:
         isbn = request.form.get("isbn")
@@ -226,24 +216,19 @@ def add():
         return render_template("addstock.html", **book_details)
 
 
-@app.route("/addstock", methods=["GET", "POST"])
+@app.route("/addstock", methods=["POST"])
 @login_required
 def addstock():
-    """Get stock quote."""
-    if request.method == "GET":
-        # Display form for user to enter stock to search
-        return render_template("add.html")
-    else:
-        isbn = request.form.get("isbn")
-        title = request.form.get("title")
-        author = request.form.get("author")
-        year = request.form.get("year")
-        copies = request.form.get("copies")
-
-        db.execute(
-            "INSERT INTO book(isbn, title, author, year, copies) VALUES(%s, %s, %s, %s, %s)", isbn, title, author, year, copies)
-        flash(f"{title} added")
+    try:
+        book = Book(request.form.get("isbn"), request.form.get("title"), request.form.get("author"),
+                    request.form.get("year"), request.form.get("copies"))
+        db.execute("INSERT INTO book(isbn, title, author, year, copies) VALUES(%s, %s, %s, %s, %s)",
+                   book.isbn, book.title, book.author, book.year, book.copies)
+        flash(f"{book.title} added")
         return redirect("/")
+    except IncompleteBookError as e:
+        flash(e)
+        return render_template("add.html")
 
 
 def errorhandler(e):
@@ -251,6 +236,11 @@ def errorhandler(e):
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
+
+
+def select_book(isbn):
+    book_records = db.execute("SELECT * FROM book WHERE isbn = %s", isbn)
+    return None if not book_records else Book(book_records[0])
 
 
 # Listen for errors
